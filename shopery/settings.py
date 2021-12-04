e40a37e7b448daa15f86ff2e4a8ac0807cfd2ee8
AytanRustamova <aytanrustamzada@gmail.com>
2021-12-04 10:23:13 +0300

    template inheritance

--- shopery/settings.py
+++ shopery/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'shopery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
