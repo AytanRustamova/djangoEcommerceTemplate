5a9d9325cdd0d76ec0aa753fbe801d47c14982a5
AytanRustamova <aytanrustamzada@gmail.com>
2021-12-04 09:56:29 +0300

    registered apps

--- shopery/settings.py
+++ shopery/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'about',
+    'blog',
+    'contact',
+    'home',
+    'order',
+    'pages',
+    'product',
+    'user'
 ]
 
 MIDDLEWARE = [
