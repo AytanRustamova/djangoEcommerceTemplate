65206ad7bcabe8db4d2a6c1f0322108694683921
AytanRustamova <aytanrustamzada@gmail.com>
2021-12-07 22:47:44 +0300

    pages app views

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def error(request):
+    return render(request, '404.html')
+
+def account_setting(request):
+    return render(request, 'account-setting.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+def shopping_cart(request):
+    return render(request, 'shopping-cart.html')
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
