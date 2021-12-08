3636931fbe53605ecd246818d53f10c056584870
AytanRustamova <aytanrustamzada@gmail.com>
2021-12-08 13:24:41 +0300

    user app views

--- shopery/urls.py
+++ shopery/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('contact/', include("contact.urls", namespace='contact')),
     path('product/', include("product.urls", namespace='product')),
     path('pages/', include("pages.urls", namespace='pages')),
+    path('account/', include("user.urls", namespace='user')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
