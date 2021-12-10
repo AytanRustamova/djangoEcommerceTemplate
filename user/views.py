378be706b89ae01f87298f38353609cdb5918aeb
AytanRustamova <aytanrustamzada@gmail.com>
2021-12-10 17:28:44 +0300

    notes.txt

--- user/views.py
+++ user/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+# User app views
+
+from django.shortcuts import redirect, render
 from . forms import RegistrationForm
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+from django.urls import reverse_lazy
+
+User = get_user_model()
 
 # Create your views here.
 
 def register(request):
     form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(data=request.POST, files=request.Files)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password = form.cleaned_data.get('password1')
+            user.save()
+            messages.success(request, 'You are successfully registered.')
+            return redirect(reverse_lazy('home:home'))
+    
     context = {
         'form' : form
     }
