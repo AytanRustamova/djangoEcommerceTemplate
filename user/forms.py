bbbf61fc3423be456de53199b8cc61649f073e0c
AytanRustamova <aytanrustamzada@gmail.com>
2021-12-10 16:01:06 +0300

    password widget

--- user/forms.py
+++ user/forms.py
@@ -3,11 +3,23 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password #Djangonun validate_password funksiyasini oz formumuzda istifade edirik.
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import password_validation
 User = get_user_model()
 
 class RegistrationForm(forms.ModelForm):
-    password1 = forms.CharField(max_length=127, validators=(validate_password,))
-    password2 = forms.CharField(max_length=127, validators=(validate_password,))
+    password1 = forms.CharField(
+        label=("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control', 'placeholder': 'New Password'}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=("Password confirmation"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control', 'placeholder': 'Confirm Password'}),
+        strip=False,
+        help_text=("Enter the same password as before, for verification."),
+    )
+
     
     class Meta:
         model = User
