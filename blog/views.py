60a444baf4aa3a23c69fe30dbae59d0458f587fc
AytanRustamova <aytanrustamzada@gmail.com>
2021-12-24 14:24:41 +0300

    Blog class based view is ready

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,18 @@
-from django.http import request
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+# from django.http import request
+# from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
-
+from .models import Blog
 # Create your views here.
 # @login_required
 # def blog(request):
 #     return render(request, "blog-list.html")
 
-@login_required
-def blog_details(request):
-    return render(request, "single-blog.html")
+# @login_required
+# def blog_details(request):
+#     return render(request, "single-blog.html")
 
 
 class BlogListView(ListView):
-    model = 'Blog'
+    model = Blog
     template_name = 'blog-list.html'
--- shopery/urls.py
+++ shopery/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from blog.models import Blog
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
