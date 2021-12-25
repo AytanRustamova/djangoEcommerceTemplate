5db7bddda929fe770280cdb7506d2bfb9f22b8d3
AytanRustamova <aytanrustamzada@gmail.com>
2021-12-25 10:35:44 +0300

    create order model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+    title = models.CharField(max_length=100,)
+    owner = models.IntegerField()
+    
+    
+    
+class OrderItem(models.Model):
+    title = models.CharField(max_length=100,)
+    order = models.IntegerField()
+    product = models.IntegerField()
+    
